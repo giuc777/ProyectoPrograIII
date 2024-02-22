21a88c0265e6b81069bc7952fd432730d12d3baa
Gadiel Us <gadyus18@gmail.com>
2024-02-21 18:55:52 -0600

    Se union, la pila con la cola, solo falta terminar el diseño

--- BancoAtenciónACliente/BancoAtenciónACliente.cpp
+++ BancoAtenciónACliente/BancoAtenciónACliente.cpp
@@ -13,16 +13,22 @@ struct nodo {
     nodo* siguiente;
 };
 
-void pop(nodo*& pila, int* id);
+void operacionesPila(nodo* pila);
+void operacionesCola(nodo* cabecera, nodo* ultimo);
+void pop(nodo*& inicio, int* id, bool esCola);
 void push(nodo*& pila, int*id);
+void pushCola(nodo*& primero, nodo*& ultimo, int* id);
 int menu();
-void RecorerPila(nodo*& pila, int* id);
+int submenu();
+void Recorer(nodo*& pila, int* id, bool esCola);
 void ModificarDatoPila(nodo*& pila, int* id);
 void buscarDatoPila(nodo*& pila, int* id);
 
 int main()
 {
-    nodo* pila = NULL;
+    nodo* pila = NULL, *primero=NULL, *ultimo = NULL;
+    
+
     int opcion, id = 0;
     do {
         opcion = menu();
@@ -30,14 +36,39 @@ int main()
         switch (opcion) {
         case 1:
             
+            operacionesPila(pila);
+            break;
+            // Aquí puedes agregar más casos para más opciones
+        case 2:
+            operacionesCola(primero, ultimo);
+            break;
+        default:
+            cout << "Opción no válida." << endl;
+        }
+    } while (opcion != 6);
+
+    
+}
+
+void operacionesPila(nodo * pila) {
+    int operation;
+    int id = 0;
+    bool cola = false;
+    
+    do {
+        operation = submenu();
+
+        switch (operation) {
+        case 1:
+
             push(pila, &id);
             break;
             // Aquí puedes agregar más casos para más opciones
         case 2:
-            RecorerPila(pila, &id);
+            Recorer(pila, &id, cola);
             break;
         case 3:
-            pop(pila, &id);
+            pop(pila, &id, cola);
             break;
         case 4:
             buscarDatoPila(pila, &id);
@@ -48,21 +79,68 @@ int main()
         default:
             cout << "Opción no válida." << endl;
         }
-    } while (opcion != 6);
+    } while (operation != 6);
 
-    
+};
+void operacionesCola(nodo* primero, nodo* ultimo) {
+    int operation;
+    int id = 0;
+    bool cola = true;
+
+    do {
+        LimpiarPatalla();
+        operation = submenu();
+
+        switch (operation) {
+        case 1:
+
+            pushCola(primero,ultimo, &id);
+            break;
+            // Aquí puedes agregar más casos para más opciones
+        case 2:
+            Recorer(primero, &id, cola);
+            break;
+        case 3:
+            pop(primero, &id, cola);
+            break;
+        case 4:
+            buscarDatoPila(primero, &id);
+            break;
+        case 5:
+            ModificarDatoPila(primero, &id);
+            break;
+        default:
+            cout << "Opción no válida." << endl;
+        }
+    } while (operation != 6);
+};
+
+
+int submenu() {
+    LimpiarPatalla();
+    int opcion1;
+    cout << "\n\tMENU" << endl;
+    cout << "1. Agregar cliente" << endl;
+    cout << "2. Mostrar clientes" << endl;
+    cout << "3. Quitar cliente" << endl;
+    cout << "4. Buscar cliente" << endl;
+    cout << "5. Modificar datos de cliente" << endl;
+    cout << "6. atras" << endl;
+    cout << "Ingrese una opcion: ";
+    cin >> opcion1;
+    return opcion1;
 }
 
 int menu() {
     LimpiarPatalla();
     int opcion;
     cout << "\n\tMENU" << endl;
-    cout << "1. Agregar elemento a la pila" << endl;
-    cout << "2. Recorrer Pila" << endl;
-    cout << "3.Salir"<<endl;
+    cout << "1. Operaciones por Pila" << endl;
+    cout << "2. Operaciones por Cola" << endl;
     cout << "Ingrese una opcion: ";
     cin >> opcion;
     return opcion;
+    
 }
 
 string convertiraMayuscula(string nombre) {
@@ -71,12 +149,14 @@ string convertiraMayuscula(string nombre) {
     }
     return nombre;
 }
-void pop(nodo*& pila, int* id) {
+void pop(nodo*& inicio, int* id, bool esCola) {
     LimpiarPatalla();
-    if (pila != NULL) { // Si la pila no está vacía
-        (*id)--;
-        nodo* aux = pila; // Creamos nodo auxiliar y le asignamos la pila
-        pila = aux->siguiente; // Movemos el inicio de la pila al siguiente nodo
+    if (inicio != NULL) { // Si la pila no está vacía
+        if (!esCola) {
+            (*id)--;
+        }
+        nodo* aux = inicio; // Creamos nodo auxiliar y le asignamos la pila
+        inicio = aux->siguiente; // Movemos el inicio de la pila al siguiente nodo
         delete aux; // Eliminamos el nodo auxiliar (el antiguo inicio de la pila)
     }
     else {
@@ -85,22 +165,21 @@ void pop(nodo*& pila, int* id) {
 
 }
 
-void ModificarDatoPila(nodo*& pila, int* id) {
+void ModificarDatoPila(nodo*& inicio, int* id) {
     LimpiarPatalla();
-    string nombre;
-    cout << "Ingrese el nombre de Usuario a modificar: ";
-    cin >> nombre;
-    nombre = convertiraMayuscula(nombre);
-    nodo* aux = pila; // Creamos nodo auxiliar y le asignamos la pila
+    int idBuscando;
+    cout << "Ingrese el id del Usuario a modificar: ";
+    cin >> idBuscando;
+    nodo* aux = inicio; // Creamos nodo auxiliar y le asignamos la pila
     nodo* encontrado = NULL;
     while (aux != NULL) { // Mientras no sea el final de la pila
-        if (aux->cliente.nombre == nombre) {
+        if (aux->cliente.id == idBuscando) {
             encontrado = aux;
         }
         aux = aux->siguiente; // Avanzamos a la siguiente posición
     }
     if (encontrado == NULL) {
-        cout << "NO hay resultados " + nombre;
+        cout << "No hay resultados " << idBuscando;
     }
     else {
         string nuevoNombre = encontrado->cliente.nombre;
@@ -112,34 +191,75 @@ void ModificarDatoPila(nodo*& pila, int* id) {
         encontrado->cliente.nombre = nuevoNombre;
         encontrado->cliente.edad = nuevaEdad;
 
+        cout << "Datos acutalizados nombre: " << encontrado->cliente.nombre << " edad: " << encontrado->cliente.edad;
+
     }
     _getch();
 }
 
 
-void buscarDatoPila(nodo *&pila, int* id) {
+void buscarDatoPila(nodo *&inicio, int* id) {
     LimpiarPatalla();
-    string nombre;
-    cout << "Ingrese el nombre de usuario ";
-    cin >> nombre;
-    
-    nodo* aux = pila; // Creamos nodo auxiliar y le asignamos la pila
-    nodo* encontrado = NULL;
-    while (aux != NULL) { // Mientras no sea el final de la pila
-        if (aux->cliente.nombre == nombre) {
-            encontrado = aux;
-        }
-        aux = aux->siguiente; // Avanzamos a la siguiente posición
-    }
-    if (encontrado == NULL) {
-        cout << "NO hay resultados "+nombre;
+    if (inicio == NULL) {
+        cout << "No hay clientes";
     }
     else {
-        cout << encontrado->cliente.nombre << " " << encontrado->cliente.edad;
+        int idUser;
+        cout << "Ingrese el id de usuario ";
+        cin >> idUser;
+        //nombre = convertiraMayuscula(nombre);
+        nodo* aux = inicio; // Creamos nodo auxiliar y le asignamos la pila
+        nodo* encontrado = NULL;
+        while (aux != NULL) { // Mientras no sea el final de la pila
+            if (aux->cliente.id == idUser) {
+                encontrado = aux;
+            }
+            aux = aux->siguiente; // Avanzamos a la siguiente posición
+        }
+        if (encontrado == NULL) {
+            cout << "No existe el cliente " << idUser;
+        }
+        else {
+            cout << encontrado->cliente.nombre << " edad: "
+                << encontrado->cliente.edad << " id: " << encontrado->cliente.id;
+        }
     }
     _getch();
 }
 
+void pushCola(nodo*& primero, nodo*& ultimo, int* id) {
+    LimpiarPatalla();
+    (*id)++;
+    string nombre;
+    int edad;
+    cout << "Ingrese nombre de cliente \n";
+    cin >> nombre;
+    for (int i = 0; i < nombre.size(); i++) {
+        nombre[i] = toupper(nombre[i]);
+    }
+    cout << "Ingrese edad de cliente \n";
+    cin >> edad;
+    nodo* nuevo_nodo = new nodo();
+    nuevo_nodo->cliente.nombre = nombre;
+    nuevo_nodo->cliente.edad = edad;
+    nuevo_nodo->cliente.id = *id;
+
+
+    if (primero == NULL) {
+        primero = nuevo_nodo;
+        primero->siguiente = NULL;
+        ultimo = primero;
+    }
+    else {
+        ultimo->siguiente = nuevo_nodo;
+        nuevo_nodo->siguiente = NULL;
+        ultimo = nuevo_nodo;
+    }
+
+    cout << "Se agrego el cliente " << nuevo_nodo->cliente.nombre << " con id: " << nuevo_nodo->cliente.id;
+    _getch();
+
+}
 
 void push(nodo*& pila, int* id) {
     LimpiarPatalla();
@@ -165,13 +285,22 @@ void push(nodo*& pila, int* id) {
     }
     pila = nuevo_nodo; // Ahora, el inicio de la pila es el nuevo nodo
 
+    cout << "Se agrego el cliente " << nuevo_nodo->cliente.nombre << " con id: " << nuevo_nodo->cliente.id;
+    _getch();
+
 }
-void RecorerPila(nodo*& pila, int* id) {
+void Recorer(nodo*& inicio, int* id, bool esCola) {
     LimpiarPatalla();
-    nodo* aux = pila; // Creamos nodo auxiliar y le asignamos la pila
+    nodo* aux = inicio; // Creamos nodo auxiliar y le asignamos la pila
 
     while (aux != NULL) { // Mientras no sea el final de la pila
-        cout << aux->cliente.nombre << " "<<aux->cliente.edad<<" id: "<<aux->cliente.id<<endl; // Imprimimos el dato
+        cout << aux->cliente.nombre << " "<<aux->cliente.edad<<" id: "<<aux->cliente.id; // Imprimimos el dato
+        if (esCola) {
+            cout << " sguiente en cola-> ";
+        }
+        else {
+            cout << endl;
+        }
         aux = aux->siguiente; // Avanzamos a la siguiente posición
     }
     system("pause");
